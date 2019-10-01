5757339f8c25e89f3f3426e41e2c2c065187cc32
marcelo.spedine <marcelo.spedine23@gmail.com>
2019-10-01 02:05:43 -0300

    Added suggestions of what to do

--- app/src/main/java/com/example/appsematron/MainActivity.java
+++ app/src/main/java/com/example/appsematron/MainActivity.java
@@ -6,6 +6,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ///features (last seen at 2:04 AM 01/10/2019)
+
+    //user area
+    //TODO Generate lists of visits and minicourses
+    //TODO generate maps of the campus and suggested places for food
+    //TODO generate list of lectures and quickbio of lecturers
+
+    //staff area
+    //TODO QRCode reading to count presence
+    //TODO sheets exhibition
+    //TODO lists of functions by hour
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
